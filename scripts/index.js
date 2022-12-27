ced96caf7cd0100e09fbba9bf4987168dfab209b
vev <vev-123@yandex.ru>
2022-12-27 22:20:08 +0500

    fin7

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
-let editButton = document.querySelector('.profile__edit-button');
+let profileEditButton = document.querySelector('.profile__edit-button');
 let popUp = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close-button')
+let popupCloseButton = document.querySelector('.popup__close-button')
 
 let popupForm = document.querySelector('.popup__form')
-let titleAdd = popupForm.querySelector('.popup__title_add-text');
-let subtitleAdd = popupForm.querySelector('.popup__subtitle_add-text');
+let userNameInput = popupForm.querySelector('.popup__input_data_name');
+let userProfessionInput = popupForm.querySelector('.popup__input_data_profession');
 
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+let profileName = document.querySelector('.profile__title');
+let profileProfession = document.querySelector('.profile__subtitle');
 
 function openPopup () {
+  userNameInput.value = profileName.textContent;
+  userProfessionInput.value = profileProfession.textContent;
   popUp.classList.add('popup_opened');
-  titleAdd.value = profileTitle.textContent;
-  subtitleAdd.value = profileSubtitle.textContent;
 }
 
 function closePopup () {
@@ -21,11 +21,11 @@ function closePopup () {
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-  profileTitle.textContent = titleAdd.value;
-  profileSubtitle.textContent = subtitleAdd.value;
+  profileName.textContent = userNameInput.value;
+  profileProfession.textContent = userProfessionInput.value;
   closePopup ();
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', handleFormSubmit);
